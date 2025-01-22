ba0c029713c9c19fdaf21eb862d20190cc713223
Shivraj-Javane <shivjavane9@gmail.com>
2025-01-22 07:09:29 +0000

    datatypes

---
+++ 01_basics/02dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as never version
+
+// alert( 2 + 2) // we are using nodejs
+
+console.log(2 + 2)
+
+console.log("Shivraj")
+
+
+let name = "Shivraj"
+let age = 25
+let isLoggedIn = true
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
+
