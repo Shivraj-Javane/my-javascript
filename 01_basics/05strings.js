6380635700acb839a2f2261c580a297ef36aca04
Shivraj Javane <shivjavane9@gmail.com>
2025-01-23 10:41:21 +0000

    strings

---
+++ 01_basics/05strings.js
@@ -0,0 +1,33 @@
+const name = "shivraj";
+const repoCount = 50;
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("shivraj-mj-com");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("i"));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-9, 4);
+console.log(anotherString);
+
+const newStringOne = "    shivraj    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://shivraj.com/shivraj%10javane";
+
+console.log(url.replace("%10", "-"));
+
+console.log(url.includes("shivraj"));
+
+console.log(gameName.split("-"));
