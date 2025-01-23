a6ef0fa49533ef4eea758a76199e028c00426f0f
Shivraj Javane <shivjavane9@gmail.com>
2025-01-23 09:39:33 +0000

    datatype summary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -32,3 +32,29 @@ const myFunction = function (){
 }
 
 console.log(typeof heros);
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "shivrajjavanedotcom"
+
+let anothername = myYoutubename
+anothername = "reactandcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "shivraj@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
