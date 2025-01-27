cc2390fa6bf63866a565fb6140265d0fa59e6820
Shivraj Javane <shivjavane9@gmail.com>
2025-01-27 09:53:08 +0000

    functions

--- 03_basics/01functions.js
+++ 03_basics/01functions.js
@@ -40,4 +40,36 @@ function loginUserMessage(username = "raj"){
 }
 
 // console.log(loginUserMessage("shivraj"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage("shivraj"))
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: "shivraj",
+    prices: 299
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "raj",
+    price: 499
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
